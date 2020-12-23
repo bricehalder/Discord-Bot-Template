4892ff208a7b5c911bd069d1a8501e8b24379cc8
Brice Halder <bhalder2@jhu.edu>
2020-12-23 16:19:45 -0500

    small code cleanup

--- commands/commands.js
+++ commands/commands.js
@@ -1,9 +1,9 @@
-const Roll = require('./roll');
-const Neko = require('./neko');
+const {Roll} = require('./roll');
+const {Neko} = require('./neko');
 
 module.exports = {
   commands: [
-    Roll.Roll,
-    Neko.Neko,
+    Roll,
+    Neko,
   ],
 };
